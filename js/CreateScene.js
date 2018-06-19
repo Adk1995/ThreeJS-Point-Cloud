0f1dab24802874efd56adc007e32e2076c59d0f8
Adhokshith <adatta9@uic.edu>
2018-06-19 11:37:33 -0500

    Added Cube with Cube rotation

--- js/CreateScene.js
+++ js/CreateScene.js
@@ -1,5 +1,20 @@
 var scene=new THREE.Scene();
-var camera=nre THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,100);
+var camera=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,100);
 var renderer=new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setSize(window.innerWidth*0.5,window.innerHeight*0.5);
 document.body.appendChild(renderer.domElement);
+
+var geometry=new THREE.BoxGeometry(1,1,1);
+var material=new THREE.MeshBasicMaterial({color: 0xff2200});
+var cube=new THREE.Mesh(geometry,material);
+scene.add(cube);
+camera.position.z=5;
+
+
+function animate() {
+	requestAnimationFrame( animate );
+  cube.rotation.x+=0.1;
+  cube.rotation.y+=0.1;
+	renderer.render( scene, camera );
+}
+animate();
