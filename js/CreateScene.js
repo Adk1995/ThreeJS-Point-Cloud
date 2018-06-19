da296f4bee80495280c9b3d31aebca6b63f867c5
Adhokshith <adatta9@uic.edu>
2018-06-19 14:18:09 -0500

    no Changes

--- js/CreateScene.js
+++ js/CreateScene.js
@@ -1,27 +1,33 @@
+
+//Initialize camera and Renderer
 var scene=new THREE.Scene();
 var camera=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,100);
 var renderer=new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth*0.5,window.innerHeight*0.5);
+renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
+//Enable Orbit Controls
+controls=new THREE.OrbitControls(camera,renderer.domElement);
 
-var geometry=new THREE.BoxGeometry(1,1,1);
+//Initilize geometry for boxes
+var geometry=new THREE.BoxGeometry(3,3,3);
 var material=new THREE.MeshBasicMaterial({color: 0xff2200});
 var cube=new THREE.Mesh(geometry,material);
 scene.add(cube);
 camera.position.z=5;
-
+//Load Dataset
+var result;
 var loader=new THREE.FileLoader();
 loader.load('data/058.csv',function(data){
-  var result=$.csv.toArrays(data);
-  console.log(result);
-});
+  result=$.csv.toArrays(data);
 
+});
+console.log(result);
 THREE.Cache.enabled=true;
 
 function animate() {
 	requestAnimationFrame( animate );
-  cube.rotation.x+=0.1;
-  cube.rotation.y+=0.1;
+  cube.rotation.x+=0.01;
+  cube.rotation.y+=0.01;
 	renderer.render( scene, camera );
 }
 animate();
