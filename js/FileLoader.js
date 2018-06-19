807fcbc0304d80ee59a5c83ea65e11e0f77fb5c5
Adhokshith <adatta9@uic.edu>
2018-06-19 13:54:12 -0500

    jquery ReadCSV

--- js/FileLoader.js
+++ js/FileLoader.js
@@ -1,6 +1,8 @@
 var loader=new THREE.FileLoader();
-
+var dataset;
 loader.load('data/058.csv',function(data){
-  console.log(data);
+  var result=$.csv.toArrays(data);
+  console.log(result);
 });
+
 THREE.Cache.enabled=true;
